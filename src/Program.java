1da2dd6f823af451780bb83677ff7c3e5a39eb03
Yeedle <abeneuwirth@gmail.com>
2016-02-09 13:35:13 -0500

    added Stage (window), Scene (scene), and Group (root0 to Program

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
+ * Program class, where the GUI stuff happens
  * Created by Yeedle on 2/2/2016.
  */
 public class Program extends Application{
@@ -9,8 +12,22 @@ public class Program extends Application{
         launch(args);
     }
 
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage window) throws Exception {
+
+
+        /* The Stage is the window. The Scene is what happens inside the window. The scene has a child node of type Group called root. */
+
+        Group root = new Group(); //root for all the children nodes to be added to the scene
+
+        Scene scene = new Scene(root, 500, 500);
+
+        window.setScene(scene);
+        window.setTitle("2048 | Java");
+
+        window.show();
 
     }
 }
