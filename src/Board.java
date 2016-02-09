4dcc64a3e0c3e3529b7d8812b35d95a40affbcc6
Yeedle <abeneuwirth@gmail.com>
2016-02-09 10:23:16 -0500

    created Board class

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Yeedle on 2/9/2016 10:10 AM.
+ */
+public class Board {
+
+    private Tile[] tiles = new Tile[16];
+
+    //TODO: We need to figure out how to keep track of spaces not occupied by tiles.
+}
