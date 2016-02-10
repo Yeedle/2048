fa4d38a51e4680a400f17e9a59616d3ca751d4d2
modicohen <mcohen34@student.touro.edu>
2016-02-10 16:22:07 -0500

    Added Score class

---
+++ src/Score.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Modi on 2/10/2016.
+ */
+public class Score {
+
+    //TODO: we need to figure out what how to construct the scores.
+    //TODO: For persistency and records keeping we can either serialize the whole program (so a player will be able to pickup exactly where he or she left), or we can just save the scores into a file.
+
+}
