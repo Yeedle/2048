5f4ff0e3df5c8f5b87fe0f0f618e536b67cd2403
Yeedle <abeneuwirth@gmail.com>
2016-02-10 18:13:03 -0500

    added TileTest class with autogenerated tests

---
+++ test/TileTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Yeedle on 2/10/2016 6:11 PM.
+ */
+public class TileTest {
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testSetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testNewValue() throws Exception {
+
+    }
+}
