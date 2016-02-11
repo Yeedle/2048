fc21c73e76c10e3065da282e1318cf41b2df146a
modicohen <mcohen34@student.touro.edu>
2016-02-10 19:37:25 -0500

    added variables and methodes to Score

--- src/Score.java
+++ src/Score.java
@@ -3,6 +3,33 @@
  */
 public class Score {
 
+    static int recordScore;
+    int currentScore;
+
+    public static int getRecordScore() {
+        return recordScore;
+    }
+
+    public static void setRecordScore(int recordScore) {
+        Score.recordScore = recordScore;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+    public Score() {
+        currentScore = 0;
+
+    }
+
+    public Score(int currentScore) {
+        this.currentScore = currentScore;
+    }
     //TODO: we need to figure out what how to construct the scores.
     //TODO: For persistency and records keeping we can either serialize the whole program (so a player will be able to pickup exactly where he or she left), or we can just save the scores into a file.
 
