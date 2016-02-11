785645e961ef2275c9298d15ae38958697d54760
Yeedle <abeneuwirth@gmail.com>
2016-02-10 19:11:17 -0500

    added empty methods: upMove(), downMove(), leftMove(), rightMove(), and initializeBoard()

--- src/Board.java
+++ src/Board.java
@@ -4,6 +4,42 @@
 public class Board {
 
     private Tile[] tiles = new Tile[16];
+    
+    public void initializeBoard() {
+        //// TODO: 2/10/2016 handle initalization of baord
+        //game starts by creating two tiles and placing them randomly on the board
+    }
+
+    
+    //the board handles the four basic moves in the game: up, down, left, and right
+    private void upMove(){
+        //TODO: handle the up move
+        
+        //after move is over, generate new tile and place in on board
+    }
+    
+    private void downMove(){
+
+        //TODO: handle the down move
+
+        //after move is over, generate new tile and place in on board
+    }
+    
+    private void leftMove(){
+
+        //TODO: handle the left move
+
+        //after move is over, generate new tile and place in on board
+    }
+    
+    private void rightMove(){
+
+        //TODO: handle the right move
+
+        //after move is over, generate new tile and place in on board
+    }
 
     //TODO: We need to figure out how to keep track of spaces not occupied by tiles.
+    
+    
 }
