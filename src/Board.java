ba92600635eab844f15d97d117d3ee5bff827670
Yeedle <abeneuwirth@gmail.com>
2016-02-11 16:55:08 -0500

    added a constructor to Board that builds the GUI with empty tiles

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,8 @@
+import javafx.geometry.Insets;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  * Created by Yeedle on 2/9/2016 10:10 AM.
@@ -7,6 +11,33 @@ public class Board extends TilePane {
 
 
     private Tile[] tiles = new Tile[16];
+
+    // constructor builds graphical componentes
+    //TODO: perphps this class can be split up into two classes (inheriting one another?), one for the logic and one for the graphics
+    public void Board(){
+        this.setHgap(15);
+        this.setVgap(15);
+        this.setPadding(new Insets(15));
+        this.setStyle("-fx-background-color: BBADA0");
+
+        //add 16 empty tiles
+        for (int i=0; i<16; i++)
+        {
+            Rectangle r = new Rectangle(106.25, 106.25);
+            r.setFill(Color.valueOf("#eee4da"));
+            r.setOpacity(0.35);
+            r.setArcWidth(7);
+            r.setArcHeight(7);
+            StackPane s = new StackPane(r);
+            this.getChildren().add(s);
+        }
+
+        this.setPrefRows(4);
+        this.setPrefColumns(4);
+
+        //after everything GUIical has been set up,
+        initializeBoard();
+    }
     
     public void initializeBoard() {
         //// TODO: 2/10/2016 handle initalization of baord
