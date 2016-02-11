090bcee091594ee9f47f2011a926685574a331c8
Yeedle <abeneuwirth@gmail.com>
2016-02-11 09:29:47 -0500

    added "extends StackPane" to Tile, added JavaFX nodes Rectangele and Text to Tile

--- src/Tile.java
+++ src/Tile.java
@@ -1,18 +1,31 @@
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 import java.util.Random;
 
 /**
  * Created by Modi on 2/9/2016.
  */
-public class Tile {
+public class Tile extends StackPane {
     int value;
     int xLocation;
     int yLocation;
+    Rectangle square = new Rectangle(25,25, Paint.valueOf("BLUE"));
+    Text valueString = new Text();
 
     public Tile(int xLocation, int yLocation) {
         this.xLocation = xLocation;
         this.yLocation = yLocation;
         this.value = newValue();
+
+        //building the Tiles graphical components
+        valueString.setText(Integer.toString(this.value));
+        this.getChildren().addAll(square, valueString);
     }
+
+
 
     public int getValue() {
         return value;
