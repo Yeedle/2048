68dfe0d1d3dd5aec5a6032897fa683b417d09b6f
modicohen <mcohen34@student.touro.edu>
2016-03-09 16:31:02 -0500

    cleaned up Model class

--- src/Model.java
+++ src/Model.java
@@ -14,12 +14,11 @@ public class Model {
     static int[][] board = new int[ROWS][COLUMNS];
 
     // constructor adds two random tiles to the board
-    public Model(){
+    public Model() {
         for (int i = 0; i < 2; i++) {
             addNewLogicalTile();
         }
     }
-
 
 
     private void addNewLogicalTile() {
@@ -35,12 +34,11 @@ public class Model {
     }
 
 
-
     private int[][] getEmptyTiles() {
         int[][] emptyArray = new int[16][2];
-        int i =0;
-        for (int row = 0; row < ROWS; row++){
-            for (int column = 0; column < COLUMNS; column++ ){
+        int i = 0;
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
                 if (board[row][column] == 0)
                     emptyArray[i] = new int[]{row, column};
                 i++;
@@ -49,36 +47,33 @@ public class Model {
         int[][] emptyTiles = new int[i][2];
         for (int j = 0; j < i; j++) {
             emptyTiles[j] = emptyArray[j];
-            }
+        }
 
         return emptyTiles;
     }
 
 
     private int newValue() {
-        if(Math.random() < 0.9 ){
+        if (Math.random() < 0.9) {
             return 2;
-        }
-        else{
+        } else {
             return 4;
         }
     }
 
 
-    static boolean move(Tile[][] logicBoard, Direction direction)
-    {
+    static boolean move(Tile[][] logicBoard, Direction direction) {
         boolean shifted;
-        switch(direction)
-        {
+        switch (direction) {
 
             case DOWN:
-                shifted= moveDown(logicBoard);
+                shifted = moveDown(logicBoard);
                 break;
             case UP:
                 shifted = moveUp(logicBoard);
                 break;
             case LEFT:
-                shifted= moveLeft(logicBoard);
+                shifted = moveLeft(logicBoard);
                 break;
             case RIGHT:
                 shifted = moveRight(logicBoard);
@@ -102,8 +97,7 @@ public class Model {
         return shifted;
     }
 
-    static boolean moveDown(Tile[][] logicBoard)
-    {
+    static boolean moveDown(Tile[][] logicBoard) {
         boolean shifted;
         logicBoard = rotateLogicBoard(logicBoard);
         shifted = shiftBoard(logicBoard);
@@ -113,8 +107,7 @@ public class Model {
         return shifted;
     }
 
-    static boolean moveRight(Tile[][] logicBoard)
-    {
+    static boolean moveRight(Tile[][] logicBoard) {
         boolean shifted;
         logicBoard = rotateLogicBoard(logicBoard);
         logicBoard = rotateLogicBoard(logicBoard);
@@ -124,8 +117,7 @@ public class Model {
         return shifted;
     }
 
-    static boolean moveUp(Tile[][] logicBoard)
-    {
+    static boolean moveUp(Tile[][] logicBoard) {
         boolean shifted;
         logicBoard = rotateLogicBoard(logicBoard);
         logicBoard = rotateLogicBoard(logicBoard);
@@ -134,14 +126,13 @@ public class Model {
         logicBoard = rotateLogicBoard(logicBoard);
         return shifted;
     }
-
 
 
     // call shiftRaw() for each row in the board
     static boolean shiftBoard(Tile[][] board) {
         boolean boardShifted = false;
         for (Tile[] row : board) {
-            if(mergeRow(row)){
+            if (mergeRow(row)) {
                 boardShifted = true;
             }
         }
@@ -160,13 +151,13 @@ public class Model {
                             targetPosition = j;
                             stop = j;
                             break;
-                        }else {
+                        } else {
                             continue;
                         }
                     } else {
                         if (row[j].getValue() == row[i].getValue()) {
                             targetPosition = j;
-                            stop = j+1;
+                            stop = j + 1;
                             break;
                         } else {
                             targetPosition = j + 1;
@@ -176,8 +167,7 @@ public class Model {
                     }
                 }
                 if (targetPosition != i && targetPosition != -1) {
-                    if (row[targetPosition].getValue() != 0)
-                    {
+                    if (row[targetPosition].getValue() != 0) {
                         row[targetPosition].wasCombinated();
                     }
                     row[targetPosition].setValue(row[i].getValue() + row[targetPosition].getValue());
@@ -195,7 +185,7 @@ public class Model {
     static boolean checkPossibleMerge(Tile[][] logicBoard) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-                if (logicBoard[i][j].getValue() == logicBoard[i][j+1].getValue()) {
+                if (logicBoard[i][j].getValue() == logicBoard[i][j + 1].getValue()) {
                     return true;
                 }
             }
@@ -203,7 +193,7 @@ public class Model {
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-                if (logicBoard[j][i].getValue() == logicBoard[j+1][i].getValue()) {
+                if (logicBoard[j][i].getValue() == logicBoard[j + 1][i].getValue()) {
                     return true;
                 }
             }
@@ -211,187 +201,14 @@ public class Model {
         return false;
     }
 
-    //************methods to handle moves down**************//
-
-    /*static boolean moveDown(Tile[][] logicBoard)
-    {
-        return shiftBoardDown(logicBoard);
-    }
-
-    // call shiftRaw() for each row in the board
-    static boolean shiftBoardDown(Tile[][] logicBoard) {
-        boolean boardShifted = false;
-        logicBoard = rotateLogicBoard(logicBoard);
-        for (Tile[] row : logicBoard) {
-            if(mergeRowDown(row)){
-                boardShifted = true;
-            }
-        }
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        return boardShifted;
-    }
-
-    // merge similar values
-    static boolean mergeRowDown(Tile[] row) {
-        boolean merged = shiftZerosDown(row);
-        for (int i = 0; i < row.length - 1; i++) {
-            if (row[i].getValue() == row[i+1].getValue() && (row[i].getValue() != 0)) {
-                row[i].setValue(row[i].getValue() + row[i+1].getValue());
-                row[i+1].setTransition(row[i+1].getTransition() + 1);
-                row[i+1].setValue(0);
-                merged = true;
-            }
-            shiftZerosDown(row);
-        }
-        return merged;
-    }
-
-    // shifts 0s to the opposite direction
-    static boolean shiftZerosDown(Tile[] row) {
-        boolean shifted = false;
-        for (int i = 0; i < row.length; i++) {
-            if (row[i].getValue() == 0) {
-                for (int j = i + 1; j < row.length; j++) {
-                    if (row[j].getValue() != 0) {
-                        Tile tempTile = row[i];
-                        row[i] = row[j];
-                        row[i].setTransition(row[i].getTransition() + Math.abs(j - i));
-                        row[j] = tempTile;
-                        break;
-                    }
-                }
-                shifted = true;
-            }
-        }
-        return shifted;
-    }*/
-
-    //************methods to handle moves right**************//
-
-   /* static  boolean moveRight(Tile[][] logicBoard)
-    {
-        return shiftBoardDown(logicBoard);
-    }
-
-    // call shiftRaw() for each row in the board
-    static boolean shiftBoardRight(Tile[][] logicBoard) {
-        boolean boardShifted = false;
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        for (Tile[] row : logicBoard) {
-            if(mergeRowRight(row)){
-                boardShifted = true;
-            }
-        }
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        return boardShifted;
-    }
-
-    // merge similar values
-    static boolean mergeRowRight(Tile[] row) {
-        boolean merged = shiftZerosRight(row);
-        for (int i = 0; i < row.length - 1; i++) {
-            if (row[i].getValue() == row[i+1].getValue() && (row[i].getValue() != 0)) {
-                row[i].setValue(row[i].getValue() + row[i+1].getValue());
-                row[i+1].setTransition(row[i+1].getTransition() + 1);
-                row[i+1].setValue(0);
-                merged = true;
-            }
-            shiftZerosRight(row);
-        }
-        return merged;
-    }
-
-    // shifts 0s to the opposite direction
-    static boolean shiftZerosRight(Tile[] row) {
-        boolean shifted = false;
-        for (int i = 0; i < row.length; i++) {
-            if (row[i].getValue() == 0) {
-                for (int j = i + 1; j < row.length; j++) {
-                    if (row[j].getValue() != 0) {
-                        Tile tempTile = row[i];
-                        row[i] = row[j];
-                        row[i].setTransition(row[i].getTransition() + Math.abs(j - i));
-                        row[j] = tempTile;
-                        break;
-                    }
-                }
-                shifted = true;
-            }
-        }
-        return shifted;
-    }
-
-    /*//************methods to handle moves up**************//*/
-
-    static boolean moveUp(Tile[][] logicBoard)
-    {
-        return shiftBoardUp(logicBoard);
-    }
-
-    // call shiftRaw() for each row in the board
-    static boolean shiftBoardUp(Tile[][] logicBoard) {
-        boolean boardShifted = false;
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        logicBoard = rotateLogicBoard(logicBoard);
-        for (Tile[] row : logicBoard) {
-            if(mergeRowUp(row)){
-                boardShifted = true;
-            }
-        }
-        logicBoard = rotateLogicBoard(logicBoard);
-        return boardShifted;
-    }
-
-    // merge similar values
-    static boolean mergeRowUp(Tile[] row) {
-        boolean merged = shiftZerosUp(row);
-        for (int i = 0; i < row.length - 1; i++) {
-            if (row[i].getValue() == row[i+1].getValue() && (row[i].getValue() != 0)) {
-                row[i].setValue(row[i].getValue() + row[i+1].getValue());
-                row[i+1].setTransition(row[i+1].getTransition() + 1);
-                row[i+1].setValue(0);
-                merged = true;
-            }
-            shiftZerosUp(row);
-        }
-        return merged;
-    }
-
-    // shifts 0s to the opposite direction
-    static boolean shiftZerosUp(Tile[] row) {
-        boolean shifted = false;
-        for (int i = 0; i < row.length; i++) {
-            if (row[i].getValue() == 0) {
-                for (int j = i + 1; j < row.length; j++) {
-                    if (row[j].getValue() != 0) {
-                        Tile tempTile = row[i];
-                        row[i] = row[j];
-                        row[i].setTransition(row[i].getTransition() + Math.abs(j - i));
-                        row[j] = tempTile;
-                        break;
-                    }
-                }
-                shifted = true;
-            }
-        }
-        return shifted;
-    }
-
-    /*//**************************//*/
-*/
     //rotate array 90 degrees clockwise
-    static Tile[][] rotateLogicBoard(Tile[][] logicBoard){
+    static Tile[][] rotateLogicBoard(Tile[][] logicBoard) {
         int i = board.length;
         int j = board[0].length;
         Tile[][] rotatedArray = new Tile[j][i];
         for (int r = 0; r < i; r++) {
             for (int c = 0; c < j; c++) {
-                rotatedArray[c][i-1-r] = logicBoard[r][c];
+                rotatedArray[c][i - 1 - r] = logicBoard[r][c];
             }
         }
         return rotatedArray;
@@ -402,50 +219,9 @@ public class Model {
         System.out.println("");
         for (Tile[] row : prntboard) {
             for (Tile j : row) {
-                System.out.print("[" + j.getValue() + "]  " + j.getTransition()+ " | ");
+                System.out.print("[" + j.getValue() + "]  " + j.getTransition() + " | ");
             }
             System.out.println("");
         }
-    }
-
-    public static void main(String[] args) {
-        Random rand = new Random();
-
-        // populate the board with random 2s and 4s for testing purposes
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (rand.nextBoolean()) {
-                    board[i][j] = 2;
-                } else {
-                    board[i][j] = 4;
-                }
-            }
-        }
-/*
-        // populate the board with random 2s and 4s for testing purposes
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (rand.nextDouble() < .2) {
-                    board[i][j] = new Tile();
-                    board[i][j].setValue(0);
-                }
-                else if(rand.nextDouble() < .4){
-                    board[i][j] = new Tile();
-                    board[i][j].setValue(4);
-                }
-                else if (rand.nextDouble() < 1){
-                    board[i][j] = new Tile();
-                    board[i][j].setValue(2);
-
-                }
-            }
-        }
-
-
-        prntBoard(board);
-        moveLeft(board);
-
-        prntBoard(board);*/
-
     }
 }
